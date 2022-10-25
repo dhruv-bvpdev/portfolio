4f2874c3fdad8505eb40da78bd7082235acf4c0c
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-10-25 17:47:14 +0530

    fix: :fire: removing preact

--- next.config.js
+++ next.config.js
@@ -4,19 +4,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
-  }
+  swcMinify: true
 }
 
 module.exports = withContentlayer(nextConfig)
