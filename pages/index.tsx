ad0a5c3aa34eeb0aa689e1f0a1dcb25bdfdb3de3
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-10-27 20:08:58 +0530

    chore: :recycle: update ui, add content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from 'react'
+import Link from 'next/link'
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import Layout from '@/components/layout'
 import Project from '@/components/projects'
+import BlogPostCard from '@/components/blog/BlogPostCard'
 import type { Projects } from '@/lib/types'
 import profilePic from '../public/static/images/PP.jpg'
 
@@ -27,63 +30,74 @@ export default function Index({
 }): JSX.Element {
   return (
     <Layout>
-      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-        <div className="w-64 h-64 mx-auto mb-16">
-          <Image
-            className="rounded-full"
-            src={profilePic}
-            placeholder="blur"
-            alt="Profile picture"
-            width="500"
-            height="500"
-            quality={100}
-            loading="lazy"
-          />
-        </div>
+      <Suspense fallback={null}>
+        <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+          <div className="flex flex-col-reverse items-start sm:flex-row">
+            <div className="flex flex-col pr-8">
+              <h1 className="mb-1 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+                Dhruv Gursahani
+              </h1>
+              <h2 className="mb-4 text-gray-700 dark:text-gray-200">
+                MBA Student at{' '}
+                <span className="font-semibold">
+                  Fore School of Management, New Delhi
+                </span>
+              </h2>
+              <p className="mb-16 text-gray-600 dark:text-gray-400">
+                I&apos;m also a developer, writer and creator. You&apos;ve found
+                my personal slice of the internet –&nbsp;sign my guestbook while
+                you&apos;re here or learn more about me.
+              </p>
+            </div>
+            <div className="relative mb-8 mr-auto w-[80px] sm:w-[176px] sm:mb-0">
+              <Image
+                alt="Dhruv Gursahani"
+                height={500}
+                width={500}
+                src={profilePic}
+                placeholder="blur"
+                sizes="30vw"
+                priority
+                className="rounded-full"
+              />
+            </div>
+          </div>
 
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-          Hey, I&apos;m Dhruv Gursahani
-        </h1>
-        <h2 className="mb-16 text-gray-600 dark:text-[#c2c2c2]">
-          I&apos;m a MBA student, developer, writer, and creator. I am a student
-          at Fore School of Management, New Delhi. You’ve found my personal
-          slice of the internet –&nbsp;sign my guestbook while you&apos;re here
-          or learn more about me.
-        </h2>
-        <h3
-          id="about"
-          className="mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white"
-        >
-          About
-        </h3>
-        <h2 className="mb-16 text-gray-600 dark:text-[#c2c2c2]">
-          <p className="mb-6">
-            Hey, I&apos;m Dhruv. I&apos;m {new Date().getFullYear() - 2001}{' '}
-            years old and come from India. I live in New Delhi and spend my free
-            time playing sports, listening to music or enjoying time with
-            friends and family.
-          </p>
-          <p className="mb-6">
-            I started working with computers when I was young and started
-            programming in school.
-          </p>
-          <p className="mb-6">
-            I&apos;m currently learning .NET Core but my favourite framework is
-            NextJs. In the future I want to build my own Kubernetes cluster with
-            backend services, database integration and a solid frontend combined
-            by Nginx.
-          </p>
-        </h2>
-        <h3
-          id="projects"
-          className="mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white"
-        >
-          Projects
-        </h3>
-        <h2 className="text-gray-600 dark:text-gray-200">
-          <Project fallbackData={fallbackData} />
-        </h2>
-      </div>
+          <h3 className="mb-6 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+            Featured Posts
+          </h3>
+          <div className="flex flex-col mb-16 gap-6 md:flex-row">
+            <BlogPostCard
+              title="Initial Commit"
+              slug="initial-commit"
+              gradient="from-[#D8B4FE] to-[#818CF8]"
+            />
+            <BlogPostCard
+              title="Best Terminal Setup"
+              slug="terminal-setup"
+              gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
+            />
+            <BlogPostCard
+              title="Best Terminal Setup"
+              slug="terminal-setup"
+              gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+            />
+          </div>
+
+          <h3
+            id="projects"
+            className="mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white"
+          >
+            Projects
+          </h3>
+          <h2 className="text-gray-600 dark:text-gray-200">
+            <Project fallbackData={fallbackData} />
+          </h2>
+          <div className="underline underline-offset-1">
+            <Link href="/projects">See more</Link>
+          </div>
+        </div>
+      </Suspense>
     </Layout>
   )
 }
