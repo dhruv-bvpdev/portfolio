27ce5865e14553625f889610228356e485623bf6
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-10-27 18:46:42 +0530

    fix: :ambulance: reverting now-playing api back to serverless from edge due to crash

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -1,35 +1,24 @@
-import { type NextRequest } from 'next/server'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { getNowPlaying } from 'lib/spotify'
 
-export const config = {
-  runtime: 'experimental-edge'
-}
-
-export default async function handler(req: NextRequest) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== 'GET') {
-    return new Response('Method not allowed', { status: 405 })
+    return res.status(405).json('Only GET method allowed')
   }
 
   const response = await getNowPlaying()
 
   if (response.status === 204 || response.status > 400) {
-    return new Response(JSON.stringify({ isPlaying: false }), {
-      status: 200,
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
+    return res.status(200).json({ isPlaying: false })
   }
 
   const song = await response.json()
 
   if (song.item === null) {
-    return new Response(JSON.stringify({ isPlaying: false }), {
-      status: 200,
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
+    return res.status(200).json({ isPlaying: false })
   }
 
   const isPlaying = song.is_playing
@@ -42,21 +31,17 @@ export default async function handler(req: NextRequest) {
   const albumImageUrl = song.item.album.images[0].url
   const songUrl = song.item.external_urls.spotify
 
-  return new Response(
-    JSON.stringify({
-      album,
-      albumImageUrl,
-      artist,
-      isPlaying,
-      songUrl,
-      title
-    }),
-    {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-        'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
-      }
-    }
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=60, stale-while-revalidate=30'
   )
+
+  return res.status(200).json({
+    album,
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title
+  })
 }
