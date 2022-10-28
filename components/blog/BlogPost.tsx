376a77e9e97263710d03f50814c9b75bc49f29cc
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-10-28 20:51:41 +0530

    feat: :dizzy: added filter blog by tags

--- components/blog/BlogPost.tsx
+++ components/blog/BlogPost.tsx
@@ -16,34 +16,68 @@ export default function BlogPost({
   const views = data?.total
 
   return (
-    <Link href={`/blog/${slug}`}>
-      <a className="w-full">
-        <div className="w-full mb-8">
-          <div className="flex flex-col justify-between md:flex-row">
-            <h3 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+    <div className="w-full">
+      <div className="mb-8 w-full">
+        <div className="flex flex-col justify-between md:flex-row">
+          <Link href={`/blog/${slug}`}>
+            <a className="mb-2 w-full text-lg font-medium text-gray-900 dark:text-gray-100 md:text-xl">
               {title}
-            </h3>
-            <p className="w-full mb-4 text-left text-gray-900 dark:text-[#c2c2c2] md:text-right md:mb-0">
-              {`${views ? new Number(views).toLocaleString() : '–––'} views`} |{' '}
-              {format(parseISO(publishedAt), 'MMMM dd, yyyy')}
-            </p>
-          </div>
-          <p className="text-gray-600 dark:text-[#c2c2c2]">{summary}</p>
-          <div className="flex mt-1">
-            {tags &&
-              tags.split(', ').map((tag, key) => {
-                return (
-                  <div
-                    key={key}
-                    className="mx-2 text-purple-700 dark:text-purple-500"
-                  >
-                    {tag}
-                  </div>
-                )
-              })}
-          </div>
+            </a>
+          </Link>
+          <p className="mb-4 min-w-fit max-w-full text-left text-gray-900 dark:text-[#c2c2c2] md:mb-0 md:text-right">
+            {`${views ? new Number(views).toLocaleString() : '–––'} views`} |{' '}
+            {format(parseISO(publishedAt), 'MMMM dd, yyyy')}
+          </p>
         </div>
-      </a>
-    </Link>
+        <p className="text-gray-600 dark:text-[#c2c2c2]">{summary}</p>
+        <div className="mt-1 flex">
+          {tags &&
+            tags.split(',').map((tag, key) => {
+              return (
+                <div
+                  key={key}
+                  className="mx-2 text-purple-700 dark:text-purple-500"
+                >
+                  <Link href={`/blog?search=${tag}&filter=tag`} replace shallow>
+                    {tag}
+                  </Link>
+                </div>
+              )
+            })}
+        </div>
+      </div>
+    </div>
   )
+}
+
+{
+  /* <Link href={`/blog/${slug}`}>
+<a className="w-full">
+  <div className="w-full mb-8">
+    <div className="flex flex-col justify-between md:flex-row">
+      <h3 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+        {title}
+      </h3>
+      <p className="w-full mb-4 text-left text-gray-900 dark:text-[#c2c2c2] md:text-right md:mb-0">
+        {`${views ? new Number(views).toLocaleString() : '–––'} views`} |{' '}
+        {format(parseISO(publishedAt), 'MMMM dd, yyyy')}
+      </p>
+    </div>
+    <p className="text-gray-600 dark:text-[#c2c2c2]">{summary}</p>
+    <div className="flex mt-1">
+      {tags &&
+        tags.split(', ').map((tag, key) => {
+          return (
+            <div
+              key={key}
+              className="mx-2 text-purple-700 dark:text-purple-500"
+            >
+              {tag}
+            </div>
+          )
+        })}
+    </div>
+  </div>
+</a>
+</Link> */
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,18 +1,45 @@
-import type { InferGetStaticPropsType } from 'next'
-import { useState } from 'react'
+import { useMemo, useState, useEffect, Suspense } from 'react'
+import { useRouter } from 'next/router'
 import { allBlogs } from 'contentlayer/generated'
 import Layout from '@/components/layout'
 import BlogPost from '@/components/blog/BlogPost'
 import { pick } from 'contentlayer/client'
 
-export default function Blog({
-  posts
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [searchValue, setSearchValue] = useState('')
+type Post = {
+  publishedAt: string
+  slug: string
+  summary: string
+  tags: string
+  title: string
+}
 
-  const filteredBlogPosts = posts.filter(post =>
-    post.title.toLowerCase().includes(searchValue.toLowerCase())
-  )
+export default function Blog({ posts }: { posts: Post[] }) {
+  const { query } = useRouter()
+  const [searchValue, setSearchValue] = useState('')
+  const [showDropdown, setShowDropdown] = useState(false)
+  const [filterBy, setFilterBy] = useState<'name' | 'tag'>('name')
+
+  useEffect(() => {
+    if (query.search) {
+      setSearchValue(query.search.toString())
+    }
+    if (query.filter) {
+      setFilterBy(query.filter === 'tag' ? 'tag' : 'name')
+    }
+  }, [query])
+
+  const filteredBlogPosts = useMemo(() => {
+    return posts.filter(post => {
+      if (filterBy === 'name') {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase())
+      }
+      if (filterBy === 'tag') {
+        return post.tags
+          .split(',')
+          .some(tag => tag.toLowerCase().includes(searchValue.toLowerCase()))
+      }
+    })
+  }, [posts, filterBy, searchValue])
 
   return (
     <Layout
@@ -33,13 +60,15 @@ export default function Blog({
             type="text"
             onChange={e => setSearchValue(e.target.value)}
             placeholder="Search articles"
-            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            defaultValue={searchValue}
+            className="block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-800 dark:bg-gray-700 dark:text-gray-100"
           />
           <svg
-            className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+            className="absolute right-3 top-3 h-5 w-5 cursor-pointer text-gray-400 dark:text-gray-300"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
+            onClick={() => setShowDropdown(!showDropdown)}
             stroke="currentColor"
           >
             <path
@@ -49,18 +78,53 @@ export default function Blog({
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
+          <div
+            className={`absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-gray-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800 dark:ring-gray-700 ${
+              !showDropdown && 'hidden'
+            }`}
+          >
+            <div className="py-1">
+              <p className="text-semibold mb-1 block border-b px-4 py-2 text-sm text-gray-700 dark:border-gray-300 dark:text-gray-200">
+                Filter
+              </p>
+              <div
+                className={`block cursor-pointer px-4 py-2 text-sm text-gray-700 dark:text-gray-200 ${
+                  filterBy === 'name' && 'font-bold'
+                }`}
+                onClick={() => {
+                  setFilterBy('name')
+                  setShowDropdown(false)
+                }}
+              >
+                Name
+              </div>
+              <div
+                className={`block cursor-pointer px-4 py-2 text-sm text-gray-700 dark:text-gray-200 ${
+                  filterBy === 'tag' && 'font-bold'
+                }`}
+                onClick={() => {
+                  setFilterBy('tag')
+                  setShowDropdown(false)
+                }}
+              >
+                Tag
+              </div>
+            </div>
+          </div>
         </div>
-        <h2 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
-          All Posts
-        </h2>
-        {!filteredBlogPosts.length && (
-          <p className="mb-4 text-gray-600 dark:text-[#c2c2c2]">
-            No posts found.
-          </p>
-        )}
-        {filteredBlogPosts.map(post => (
-          <BlogPost key={post.title} {...post} />
-        ))}
+        <Suspense fallback={null}>
+          <h2 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+            All Posts
+          </h2>
+          {!filteredBlogPosts.length && (
+            <p className="mb-4 text-gray-600 dark:text-[#c2c2c2]">
+              No posts found.
+            </p>
+          )}
+          {filteredBlogPosts.map(post => (
+            <BlogPost key={post.title} {...post} />
+          ))}
+        </Suspense>
       </div>
     </Layout>
   )
